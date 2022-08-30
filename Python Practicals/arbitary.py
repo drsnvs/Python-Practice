4defcc01be9a5bfa619142c67e7952790723f51e
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-08-30 18:34:43 +0530

    Arbitrary Argument

---
+++ Python Practicals/arbitary.py
@@ -0,0 +1,16 @@
+# def sum(*arg):
+#     sum = 0 
+#     for i in arg:
+#         sum += i
+#     return sum
+# print("New")
+# print(sum(10,20,30,40))
+# print(sum(40))
+# print(sum(10,20,30,40,60,70,80))
+
+
+lst = [10,20,30,40]
+add = 0
+for i in lst:
+    add += i
+print(add)
