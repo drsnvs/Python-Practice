ef26cac41e69102f1d56a16f18ab167c6dcf334a
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-09-09 12:51:56 +0530

    Delete

--- _9_assert.py
+++
@@ -1,8 +0,0 @@
-#Write a program to assert the user enters a number greater than zero.
-
-x = 10
-y = 0
-assert x > 0,"Only greater than 0 allowed"
-print('x is a positive number.')
-assert y > 0 ,"Only greater than 0 allowed"
-print('y is a positive number.')
