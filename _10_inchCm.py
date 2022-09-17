a4db6df2cfecd58038854e666a12908f6eb409da
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-09-17 09:56:46 +0530

    Delete

--- _10_inchCm.py
+++
@@ -1,5 +0,0 @@
-#Write a python program that asks the user to enter a length in centimetres. If the
-#user enters a negative length, the program should tell the user that the entry is
-#invalid. Otherwise, the program should convert the length to inches and print out
-#the result. (2.54 = 1 inch).
-
